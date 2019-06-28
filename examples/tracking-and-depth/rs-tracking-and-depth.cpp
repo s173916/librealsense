fd3bd9adb5995c00596507ec6540593fb4c337c9
Phillip Schmidt <phillip.schmidt@intel.com>
2019-06-28 12:43:53 -0700

    pose predict (extrapolate)

--- examples/tracking-and-depth/rs-tracking-and-depth.cpp
+++ examples/tracking-and-depth/rs-tracking-and-depth.cpp
@@ -8,6 +8,43 @@
 #include <fstream>              // std::ifstream
 #include <iomanip>
 
+#include <math.h>
+#include <float.h>
+
+inline rs2_quaternion quaternion_exp(rs2_vector v)
+{
+    float x = v.x/2, y = v.y/2, z = v.z/2, th2, th = sqrtf(th2 = x*x + y*y + z*z);
+    float c = cosf(th), s = th2 < sqrtf(120*FLT_EPSILON) ? 1-th2/6 : sinf(th)/th;
+    rs2_quaternion Q = { s*x, s*y, s*z, c };
+    return Q;
+}
+
+inline rs2_quaternion quaternion_multiply(rs2_quaternion a, rs2_quaternion b)
+{
+    rs2_quaternion Q = {
+        a.x * b.w + a.w * b.x - a.z * b.y + a.y * b.z,
+        a.y * b.w + a.z * b.x + a.w * b.y - a.x * b.z,
+        a.z * b.w - a.y * b.x + a.x * b.y + a.w * b.z,
+        a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z,
+    };
+    return Q;
+}
+
+rs2_pose predict_pose(rs2_pose & pose, float dt_s, int predict_accel = 0)
+{
+    rs2_pose P = pose;
+    P.translation.x = dt_s * (predict_accel*dt_s/2 * pose.acceleration.x + pose.velocity.x) + pose.translation.x;
+    P.translation.y = dt_s * (predict_accel*dt_s/2 * pose.acceleration.y + pose.velocity.y) + pose.translation.y;
+    P.translation.z = dt_s * (predict_accel*dt_s/2 * pose.acceleration.z + pose.velocity.z) + pose.translation.z;
+    rs2_vector W = {
+            dt_s * (predict_accel*dt_s/2 * pose.angular_acceleration.x + pose.angular_velocity.x),
+            dt_s * (predict_accel*dt_s/2 * pose.angular_acceleration.y + pose.angular_velocity.y),
+            dt_s * (predict_accel*dt_s/2 * pose.angular_acceleration.z + pose.angular_velocity.z),
+    };
+    P.rotation = quaternion_multiply(quaternion_exp(W), pose.rotation);
+    return P;
+}
+
 // Helper functions
 void register_glfw_callbacks(window& app, glfw_state& app_state);
 
@@ -107,9 +144,16 @@ int main(int argc, char * argv[]) try
         if (points && pose_frame) {
             //timing
             double ts_pose = pose_frame.get_timestamp();
-            std::cout << "ts_{depth,pose}_ms: " << std::setprecision(16) << ts_depth << ", " << ts_pose << " dt_ms (depth wrt pose) = " << ts_depth - ts_pose << std::endl;
+            double dt_ms = ts_depth - ts_pose;
+            std::cout << "ts_{depth,pose}_ms: " << std::setprecision(16) << ts_depth << ", " << ts_pose << " dt_ms (depth wrt pose) = " << dt_ms << std::endl;
             rs2_pose pose =  pose_frame.get_pose_data();
-            draw_pointcloud_wrt_world(app.width(), app.height(), app_state, points, pose, H_t265_d400);
+
+            // pose predict
+            rs2_pose pose_predicted = predict_pose(pose, dt_ms/1000);  // ms to s
+
+            //draw_pointcloud_wrt_world(app.width(), app.height(), app_state, points, pose, H_t265_d400);
+            draw_pointcloud_wrt_world(app.width(), app.height(), app_state, points, pose_predicted, H_t265_d400);
+
             rendering_counter++;
         }
 
